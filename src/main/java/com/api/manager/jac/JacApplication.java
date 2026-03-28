c07e19d0afa65e51d423529c6bb82eb165875420
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-03-28 17:46:27 -0500

    Initial Spring Boot project setup

---
+++ src/main/java/com/api/manager/jac/JacApplication.java
@@ -0,0 +1,13 @@
+package com.api.manager.jac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacApplication.class, args);
+	}
+
+}
