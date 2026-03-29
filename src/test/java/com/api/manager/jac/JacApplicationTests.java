bb6e0f7bc0fbb3cad6a68373272d9b7c6bc3e6c9
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-03-28 23:08:06 -0500

    chore: styles and adjustments are applied

--- src/test/java/com/api/manager/jac/JacApplicationTests.java
+++ src/test/java/com/api/manager/jac/JacApplicationTests.java
@@ -1,7 +1,6 @@
 package com.api.manager.jac;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * Test class for the Jac application context. This test ensures that the Spring application context
